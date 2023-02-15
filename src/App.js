514ab1263d229579a87e45b3710107ebcf75a082
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-15 19:30:10 +0530

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import "./App.css";
 function App() {
   return (
     <div className={"App"}>
-      <div>Videos</div>
+      <div className={"name"}>Videos</div>
       <Video
-        title={"React Tutorial 2"}
+        title={"React Tutorial 1"}
         views={"10K"}
         channel={"Subham Chowdhury"}
         time={"1 days ago"}
+        verified={true}
       ></Video>
       <Video
         title={"React Tutorial 2"}
         views={"200K"}
         channel={"Subham Chowdhury"}
         time={"2 days ago"}
+        verified={false}
+      ></Video>
+      <Video
+        title={"React Tutorial 2"}
+        views={"21M"}
+        channel={"Subham Chowdhury"}
+        time={"1 Year ago"}
+        verified={true}
       ></Video>
     </div>
   );
--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,13 +1,26 @@
 import "./Video.css";
 
-export default function Video({ title, channel, views, time }) {
+export default function Video({ title, channel, views, time, verified }) {
+  //   Conditional Rendering
+  // if (verified) {
+  //   channelJSX = <div className="channel">{channel} ✅</div>;
+  // } else {
+  //   channelJSX = <div className="channel">{channel}</div>;
+  // }
+
   return (
     <div className={"container"}>
       <div className="pic">
         <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
       </div>
-      <div className="title">title {title}</div>
-      <div className="channel">Channel {channel}</div>
+      <div className="title">{title}</div>
+      {/*<div className="channel">*/}
+      {/*  {channel} {verified ? "✅" : null}*/}
+      {/*</div>*/}
+      <div className="channel">
+        {channel}{" "}
+        {verified && "✅" /* This is a short hand for the above code */}
+      </div>
       <div className="views">
         {views} views <span>.</span> {time}
       </div>
