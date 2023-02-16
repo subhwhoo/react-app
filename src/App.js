1e641fbe64ffe931ad46fffe27fa64bbf0e4236c
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-16 10:47:13 +0530

    Maps

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
 import Video from "./Components/Video";
 import "./App.css";
+import videos from "./data/data";
 
 function App() {
-  let videos = [
-    {
-      id: 1,
-      title: "React Tutorial 1",
-      views: "10K",
-      channel: "Subham Chowdhury",
-      time: "1 days ago",
-      verified: true,
-    },
-    {
-      id: 2,
-      title: "React Tutorial 2",
-      views: "20K",
-      channel: "Subham Chowdhury",
-      time: "2 days ago",
-      verified: true,
-    },
-    {
-      id: 3,
-      title: "React Tutorial 3",
-      views: "30K",
-      channel: "Subham Chowdhury",
-      time: "3 days ago",
-      verified: true,
-    },
-    {
-      id: 4,
-      title: "React Tutorial 4",
-      views: "40K",
-      channel: "Subham Chowdhury",
-      time: "4 days ago",
-      verified: true,
-    },
-  ];
   return (
     <div className={"App"}>
       <div className={"name"}>Videos</div>
---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+const videos = [
+  {
+    id: 1,
+    title: "React Tutorial 1",
+    views: "10K",
+    channel: "Subham Chowdhury",
+    time: "1 days ago",
+    verified: true,
+  },
+  {
+    id: 2,
+    title: "React Tutorial 2",
+    views: "20K",
+    channel: "Subham Chowdhury",
+    time: "2 days ago",
+    verified: true,
+  },
+  {
+    id: 3,
+    title: "React Tutorial 3",
+    views: "30K",
+    channel: "Subham Chowdhury",
+    time: "3 days ago",
+    verified: true,
+  },
+  {
+    id: 4,
+    title: "React Tutorial 4",
+    views: "40K",
+    channel: "Subham Chowdhury",
+    time: "4 days ago",
+    verified: true,
+  },
+];
+
+export default videos;
