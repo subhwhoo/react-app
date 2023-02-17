d554aa7dd3db60f760bfc27e7a642c1d906d597c
Subham Chowdhury <itz_subhamrock@outlook.com>
2023-02-18 00:21:50 +0530

    Forms Update

--- src/Components/AddVideo.js
+++ src/Components/AddVideo.js
@@ -1,22 +1,50 @@
+import "./AddVideo.css";
+import { useState } from "react";
+
 export default function AddVideo() {
+  const [video, setVideo] = useState({
+    time: "5 days ago",
+    channel: "Subham Chowdhury",
+    verified: true,
+  });
+
+  function handleSubmit() {}
+
+  function handleChange(e) {
+    console.log(e.target.name, e.target.value);
+    e.stopPropagation();
+    setVideo({ ...video, [e.target.name]: e.target.value });
+  }
+
   return (
     <form>
-      <input type="text" />
-      <input type="text" />
+      <input
+        type="text"
+        name={"title"}
+        onChange={handleChange}
+        placeholder={"Title"}
+      />
+      <input
+        type="text"
+        name={"views"}
+        onChange={handleChange}
+        placeholder={"Views"}
+      />
       <button
-        onClick={() => {
-          // setVideos([
-          //   ...videos,
-          //   {
-          //     id: videos.length + 1,
-          //     title: "Demo Tutorial 5",
-          //     views: "50K",
-          //     channel: "Subham Chowdhury",
-          //     time: "5 days ago",
-          //     verified: true,
-          //   },
-          // ]);
-        }}
+        onClick={handleSubmit}
+        // onClick={() => {
+        //   setVideos([
+        //     ...videos,
+        //     {
+        //       id: videos.length + 1,
+        //       title: "Demo Tutorial 5",
+        //       views: "50K",
+        //       channel: "Subham Chowdhury",
+        //       time: "5 days ago",
+        //       verified: true,
+        //     },
+        //   ]);
+        // }}
       >
         Add Videos
       </button>
